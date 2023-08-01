6d3d10c5534e85c74601a473f65f423a05250ddc
roxmond <60523691+roxmond@users.noreply.github.com>
2023-08-01 16:32:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      'converter.js',
-      'converter.css'
+      '/',
+      '/converter.js',
+      '/converter.css'
     ]);
   })());
 });
